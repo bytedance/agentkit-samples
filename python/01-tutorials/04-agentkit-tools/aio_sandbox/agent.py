26631497ac61f846d3c4ccb26f2fd1b58a157546
李雪飞 <lixuefei.nice@bytedance.com>
2026-08-03 07:41:16 +0000

    update model name of aio sandbox
    [no-workitem]
    
    GitOrigin-RevId: 1ab227aebbb447509bc619cf2b6cea812892ebb3

--- python/01-tutorials/04-agentkit-tools/aio_sandbox/agent.py
+++ python/01-tutorials/04-agentkit-tools/aio_sandbox/agent.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from veadk import Agent, Runner
 from veadk.memory.short_term_memory import ShortTermMemory
@@ -15,7 +16,7 @@ app_name = "agent_with_runcode"
 
 agent: Agent = Agent(
     name="code_agent",
-    model_name="doubao-seed-1-6-251015",
+    model_name=os.getenv("MODEL_AGENT_NAME", "deepseek-v4-pro-260425"),
     description="A fun Python coding assistant",
     instruction="你是一个有趣的Python代码实验员。你的任务是利用沙箱环境解决各种有趣的问题。比如：通过蒙特卡洛方法模拟概率问题，生成有趣的ASCII艺术字，或者通过算法解决逻辑谜题。请尽量使用 Python 标准库，必须使用 `run_code` 工具执行代码并向用户展示结果。避免复杂的外部依赖安装。",
     tools=[run_code],
